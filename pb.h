411350cc4959d9b80d91e951600cbc85cad00f8e
avduryagin <avduryagin@mail.ru>
2023-01-22 10:45:12 +0300

    added pd.h

---
+++ pb.h
@@ -0,0 +1,7 @@
+#pragma once
+
+double distance(double& latitude0, double& longtitude0, double& latitude1, double& longtitude1);
+double haversine(double& latitude0, double& longtitude0, double& latitude1, double& longtitude1);
+double degree_to_radian(double& degree);
+double dcos(double& degree);
+double dsin(double& degree);
